f5900498587d4ee0e59f4ce731cbdfc110e8ee5e
guni973 <ad2314ce71926@gmail.com>
2017-03-27 10:13:24 +0900

    bootstrap theme change

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var twitter = require('twitter');
 var http = require('http');
 var socket = require('socket.io');
 var fs = require('fs');
-var keys = JSON.parse(fs.readFileSync('./oauthkeys.json', 'utf8'));
+var querystring = require('querystring');
 
+// OAuth認証
+var keys = JSON.parse(fs.readFileSync('./oauthkeys.json', 'utf8'));
 var client = new twitter({
     consumer_key        : keys.consumer_key,
     consumer_secret     : keys.consumer_secret,
@@ -11,10 +13,24 @@ var client = new twitter({
     access_token_secret : keys.access_token_secret,
 });
 
+
 var http_server = new http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(fs.readFileSync('./index.html', 'utf-8'));
+  // tweet処理
+  if(req.url === '/tweet' && req.method === 'POST'){
+    var body = '';
+    req.on('data', function(data){
+      body += data;
+    });
+    req.on('end', function(chunck){
+      var POST = querystring.parse(body);
+      console.log(POST);
+      console.log(POST['tweet'])
+    });
+  }
 }).listen(3000);
+
 
 var sio = socket.listen(http_server);
 
@@ -31,7 +47,6 @@ client.stream('user',
         });
       console.log(tweet.user.name);
       console.log(tweet.text);
-      console.log(tweet.user.profile_image_url)
     });
 
     stream.on('error', function(error) {
